e1a2fabefa94454d94fab4083055042e205ecc53
fernando-castelo <nando.castelobranco24@hotmail.com>
2024-06-06 19:23:26 -0300

    feat(): adding initial url controller class

---
+++ src/main/java/com/example/encurtador_link/controller/UrlController.java
@@ -0,0 +1,24 @@
+package com.example.encurtador_link.controller;
+
+import com.example.encurtador_link.entity.Url;
+import com.example.encurtador_link.service.UrlService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UrlController {
+
+    private final UrlService urlService;
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+    @PostMapping()
+    public ResponseEntity<Url> createShortenedUrl(@RequestBody Url url) {
+        Url createdUrl = urlService.createUrl(url);
+        return ResponseEntity.status(201).body(createdUrl);
+    }
+}
