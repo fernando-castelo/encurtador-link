4e4ee1423231c782c38ed7bc0759ef48e657a955
fernando-castelo <nando.castelobranco24@hotmail.com>
2024-06-06 18:22:08 -0300

    First Commit

---
+++ src/test/java/com/example/encurtador_link/EncurtadorLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.encurtador_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
