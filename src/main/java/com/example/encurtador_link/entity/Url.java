27cc487b72e80e9fbad491c21a7366632f3f8d37
fernando-castelo <nando.castelobranco24@hotmail.com>
2024-06-06 18:52:28 -0300

    feat(): add url model and repository

---
+++ src/main/java/com/example/encurtador_link/entity/Url.java
@@ -0,0 +1,64 @@
+package com.example.encurtador_link.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name= "url")
+public class Url {
+
+    @Id
+    private UUID id;
+
+    private String original_url;
+
+    private String hash;
+
+    private Date createdAt;
+
+    public Url(UUID id, String original_url, String hash, Date createdAt) {
+        this.id = id;
+        this.original_url = original_url;
+        this.hash = hash;
+        this.createdAt = createdAt;
+    }
+
+    public Url() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getOriginal_url() {
+        return original_url;
+    }
+
+    public void setOriginal_url(String original_url) {
+        this.original_url = original_url;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
---
+++ src/main/java/com/example/encurtador_link/repository/UrlRepository.java
@@ -0,0 +1,11 @@
+package com.example.encurtador_link.repository;
+
+import com.example.encurtador_link.entity.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UrlRepository extends JpaRepository<Url, UUID> {
+}
