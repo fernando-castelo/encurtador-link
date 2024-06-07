95306728926c01c6037bc8d7a0f23c6dac3ea779
fernando-castelo <nando.castelobranco24@hotmail.com>
2024-06-06 22:45:42 -0300

    feat(): add url request dto

---
+++ src/main/java/com/example/encurtador_link/dto/UrlRequestDto.java
@@ -0,0 +1,14 @@
+package com.example.encurtador_link.dto;
+
+public class UrlRequestDto {
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
